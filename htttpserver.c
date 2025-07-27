573a50c2be518c3ebc9ab92c0a9c04bdf713d228
2403246954@qq.com <2403246954@qq.com>
2025-07-27 22:15:04 +0800

    添加线程函数多线程处理工作

--- htttpserver.c
+++ htttpserver.c
@@ -1,4 +1,6 @@
 #include "net_func.h" // 导入网络层相关函数头文件
+#include "settings.h" // 导入宏
+#include "pthd_fun.h" // 导入线程相关函数
 
 int main(int argc, char const *argv[])
 {
@@ -6,16 +8,31 @@ int main(int argc, char const *argv[])
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     server_init_check(server_fd);
 
-    // 绑定
+    // 设置地址参数（用于绑定）
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = 8080;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // 端口复用
+    int opt = 1; // 1 启用，0 禁用
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+
+    // 绑定
     bind_check(bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)), server_fd);
 
     // 监听
     listen_check(listen(server_fd, 4096), server_fd);
     printf("服务器启动.....\n");
 
+    // 建立线程池来多线程
+    pthread_t pthreadpools[PTHREADMAX];
+    for (int i = 0; i < PTHREADMAX; i++)
+    {
+        pthread_create(&pthreadpools[i], NULL, work_thread, NULL);
+    }
+
+    // 关闭服务器
+    close(server_fd);
     return EXIT_SUCCESS;
 }
