0847d2eb4f1f87f0a6b60de601c6b710b848daff
2403246954@qq.com <2403246954@qq.com>
2025-07-29 14:19:55 +0800

    新增定义MIN宏

--- settings.h
+++ settings.h
@@ -1,9 +1,10 @@
 #include "head.h"
 
-#define PTHREADMAX 10  // 线程池子中最大数量
-#define MAXEVENTS 1024 // epoll实例中事件的最大数量
-#define BUFSIZE 1024   // 缓存区大小
-#define PROT 9000      // 服务器端口号
+#define PTHREADMAX 10                     // 线程池子中最大数量
+#define MAXEVENTS 1024                    // epoll实例中事件的最大数量
+#define BUFSIZE 1024                      // 缓存区大小
+#define PROT 9000                         // 服务器端口号
+#define MIN(x, y) ((x) < (y) ? (x) : (y)) // 定义MIN宏
 
 int server_fd; // 服务器套接字的文件描述符
 int epoll_fd;  // epoll的文件描述符
